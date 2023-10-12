69ce937290279e14a48be43c88d13c6a8106b6a7
Blastard23 <147681317+Blastard23@users.noreply.github.com>
2023-10-12 15:23:41 +0800

    Add files via upload

--- database/seeders/CocktailSeeder.php
+++ database/seeders/CocktailSeeder.php
@@ -14,17 +14,17 @@ class CocktailSeeder extends Seeder
     public function run(): void
     {
         Cocktail::insert([
-            ['name' => 'Margarita', 'brand' => 'Tequila Brand', 'alcohol' => 'Alcoholic'],
-            ['name' => 'Martini', 'brand' => 'Vodka Brand', 'alcohol' => 'Alcoholic'],
-            ['name' => 'Mojito', 'brand' => 'Rum Brand', 'alcohol' => 'Alcoholic'],
-            ['name' => 'Cosmopolitan', 'brand' => 'Vodka Brand', 'alcohol' => 'Alcoholic'],
-            ['name' => 'Daiquiri', 'brand' => 'Rum Brand', 'alcohol' => 'Alcoholic'],
-            ['name' => 'Pina Colada', 'brand' => 'Rum Brand', 'alcohol' => 'Alcoholic'],
-            ['name' => 'Old Fashioned', 'brand' => 'Whiskey Brand', 'alcohol' => 'Alcoholic'],
-            ['name' => 'Screwdriver', 'brand' => 'Vodka Brand', 'alcohol' => 'Alcoholic'],
-            ['name' => 'Whiskey Sour', 'brand' => 'Whiskey Brand', 'alcohol' => 'Alcoholic'],
-            ['name' => 'Negroni', 'brand' => 'Gin Brand', 'alcohol' => 'Alcoholic'],
-            ['name' => 'Virgin Mojito', 'brand' => 'Non-Alcoholic Brand', 'alcohol' => 'Non_Alcoholic'],
+            ['name' => 'Margarita', 'brand' => 'Tequila Brand', 'alcohol' => 'Alcoholic', 'category' => 'Classic'],
+            ['name' => 'Martini', 'brand' => 'Vodka Brand', 'alcohol' => 'Alcoholic', 'category' => 'Classic'],
+            ['name' => 'Mojito', 'brand' => 'Rum Brand', 'alcohol' => 'Alcoholic', 'category' => 'Classic'],
+            ['name' => 'Cosmopolitan', 'brand' => 'Vodka Brand', 'alcohol' => 'Alcoholic', 'category' => 'Classic'],
+            ['name' => 'Daiquiri', 'brand' => 'Rum Brand', 'alcohol' => 'Alcoholic', 'category' => 'Classic'],
+            ['name' => 'Pina Colada', 'brand' => 'Rum Brand', 'alcohol' => 'Alcoholic', 'category' => 'Classic'],
+            ['name' => 'Old Fashioned', 'brand' => 'Whiskey Brand', 'alcohol' => 'Alcoholic', 'category' => 'Classic'],
+            ['name' => 'Screwdriver', 'brand' => 'Vodka Brand', 'alcohol' => 'Alcoholic', 'category' => 'Classic'],
+            ['name' => 'Whiskey Sour', 'brand' => 'Whiskey Brand', 'alcohol' => 'Alcoholic', 'category' => 'Classic'],
+            ['name' => 'Negroni', 'brand' => 'Gin Brand', 'alcohol' => 'Alcoholic', 'category' => 'Classic'],
+            ['name' => 'Virgin Mojito', 'brand' => 'Non-Alcoholic Brand', 'alcohol' => 'Non_Alcoholic', 'category' => 'Non_Alcoholic'],
         ]);
     }
 }
